1b1f04708b4086292c7caf6f741e987f8c504179
IhorKolisnichenko <kolisnichenkoihor7@gmail.com>
2023-10-25 02:15:04 -0400

    added data types &  services

--- react_hooks_typescript/src/App.tsx
+++ react_hooks_typescript/src/App.tsx
@@ -1,23 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          drfRest
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+      {/* <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/tutorials" element={<App />} />
+          <Route path="/add" element={<App />} />
+          <Route path="/tutorials/:id" element={<App />} />
+        </Routes>
+      </div> */}
     </div>
   );
 }
